4606fbbc921577034f87d0226e2664aad11f32af
cchompoottr <chompoo99075@gmail.com>
2024-08-01 09:18:47 +0700

    responsive sm

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -15,32 +15,32 @@ function Index() {
 
   return (
   
-    <div className="flex flex-wrap bg lg:flex flex-col lg:flex-nowrap lg:h-fit">
-      <div className='items-start -ml-10 w-1/3  mt-20 lg:w-full lg:-translate-x-2 lg:mt-20'>
-        <h1 className='title text-7xl font-bold text-white mt-20 lg:text-6xl lg:mt-10'>Find<br />Your<br />Musics</h1>
+    <div className="flex flex-wrap bg sm:flex flex-col sm:flex-nowrap sm:h-fit md:flex items-stretch">
+      <div className='items-start -ml-10 w-1/3  mt-20 sm:w-full sm:-translate-x-2 sm:mt-20 md:mr-20 md:ml-0 md:self-center'>
+        <h1 className='title text-7xl font-bold text-white mt-20 sm:text-6xl sm:mt-10 md:text-5xl'>Find<br />Your<br />Musics</h1>
 
-        <div className="input max-w-sm mx-auto mt-10 lg:translate-y-10 lg:mt-20">
-          <label htmlFor="input-label" className="block text-xl font-medium mb-5 text-white lg:text-base ">
+        <div className="input max-w-sm mx-auto mt-10 sm:translate-y-10 sm:mt-20 md:mt-20 md:mx-auto md:translate-x-[65px]">
+          <label htmlFor="input-label" className="block text-xl font-medium mb-5 text-white sm:text-base md:text-base">
             Select a music you like
           </label>
           <input
             id="input-label"
-            className="w-[350px] py-2.5 px-3 block text-white border-solid border-4 border-gray-100 rounded-lg text-lg mb-5 focus:border-violet-700  disabled:pointer-events-none bg-neutral-950 border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 lg:py-1.5 lg:w-[300px] lg:text-lg"
+            className="w-[350px] py-2.5 px-3 block text-white border-solid border-4 border-neutral-500 rounded-lg text-lg mb-5 focus:border-violet-700  disabled:pointer-events-none bg-neutral-950 sm:py-1.5 sm:w-[290px] sm:text-lg md:text-lg md:py-2 md:w-[290px] md:mt-0"
           />
         </div>
 
-        <button type="button" className="w-[350px] py-2.5 px-20 me-2 mb-2 text-xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg hover:bg-violet-700 hover:text-white focus:z-10 focus:ring-gray-100 hover:-translate-y-1 hover:scale-100 lg:py-1.5 lg:text-base lg:w-[300px] lg:mt-10">
+        <button type="button" className="w-[350px] py-2.5 px-20 me-2 mb-2 text-xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg hover:bg-violet-700 hover:text-white focus:z-10 focus:ring-gray-100 hover:-translate-y-1 hover:scale-100 sm:py-1.5 sm:text-base sm:w-[290px] sm:mt-10 md:w-[290px] md:text-base">
           Recommend
         </button>
       </div>
 
-      <div className="items-end w-2/3 lg:translate-y-20 lg:pb-[120px]">
-        <div className="grid gap-x-0 gap-y-8 grid-cols-3 mt-20 translate-y-5 ml-20 lg:grid-cols-2 lg:gap-x-[100px] lg:gap-y-5 lg:mt-20 lg:ml-10">
+      <div className="items-end w-2/3 sm:translate-y-20 sm:pb-[120px] sm:mb-20 md:self-center md:mr-20">
+        <div className="grid gap-x-0 gap-y-8 grid-cols-3 mt-20 translate-y-5 ml-20 sm:grid-cols-2 sm:gap-x-[100px] sm:gap-y-5 sm:mt-20 sm:ml-10 md:grid gap-1 md:grid-cols-2 md:ml-10 md:mr-20">
           {Array(6).fill().map((_, index) => (
-              <div key={index} className={`container bg-slate-50 h-[325px] w-[225px] rounded-lg overflow-hidden hover:bg-neutral-100 hover:ring-4 ring-violet-700 lg:h-[225px] lg:w-[150px]`}>
+              <div key={index} className={`bg-slate-50 h-[325px] w-[225px] rounded-lg overflow-hidden hover:bg-neutral-100 hover:ring-4 ring-violet-700 sm:h-[225px] sm:w-[150px] md:w-[150px] md:h-[225px]`}>
                 <img className="object-cover" src='https://i.scdn.co/image/ab67616d0000b2739abdf14e6058bd3903686148' alt="style" />
-                <h1 className="text-2xl text-center font-bold mt-5 lg:text-base mt-3">{message}</h1>
-                <h3 className="text-xl text-center lg:text-sm">{message}</h3>
+                <h1 className="text-2xl text-center font-bold mt-5 sm:text-base sm:mt-3 md:text-base">{message}</h1>
+                <h3 className="text-xl text-center sm:text-sm md:text-sm">{message}</h3>
               </div>
           ))}
         </div>
